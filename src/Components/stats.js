007a15add66e0ecdf1659bad432272ec8f25d66c
Robin Del Mundo <batbord94@gmail.com>
2023-03-15 05:08:47 +0000

    - Connected the front end into the rest api - Included a loading/setLoading hook to always load the latest fetched data - Changed site title

--- src/Components/stats.js
+++ src/Components/stats.js
@@ -13,23 +13,32 @@ const Stats = (props) => {
     return string.replace(/\W+/g, "-").toLowerCase();
   };
 
+  console.log(props.timeframes[timeframeSelected].current);
+
   const clickHandler = (title) => {
     if (props.active === title) {
       setActiveMenu(!activeMenu);
     }
-  }
+  };
 
   const removeActive = () => {
     setActiveMenu(false);
-  }
+  };
 
   return (
     <Card className={"stats " + sanitizedTitle(props.title.toLowerCase())}>
       <div className="flex-title">
         <h1 className="stat-title">{props.title}</h1>
         <div>
-          <img onClick={() => clickHandler(props.title)} className="ellipsis" src={ellipsis} />
-          <ContextMenu className={activeMenu === true ? "active " : ""} items="Edit" />
+          <img
+            onClick={() => clickHandler(props.title)}
+            className="ellipsis"
+            src={ellipsis}
+          />
+          <ContextMenu
+            className={activeMenu === true ? "active " : ""}
+            items="Edit"
+          />
         </div>
       </div>
       <h2 className="stat-hours">
